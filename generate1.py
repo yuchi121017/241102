f52a4803f053f4e014577aba9e1d38c0d28778b3
yuchi121017 <grace0222427@gmail.com>
2024-11-02 22:40:35 +0800

    241102_2240_update

--- generate1.py
+++ generate1.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import google.generativeai as genai
 import os
+import re
 
 # 加載 .env 文件並配置 API 密鑰
 load_dotenv()
@@ -11,17 +12,17 @@ if api_key is None:
 
 genai.configure(api_key=api_key)
 
-def analyze_content(content):
+def analyze_content(content, prompt="請分析以下內容，並列出關於這項產品的優缺點，列點表示"):
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(content)
     
-    # 檢查返回結果的結構
-    # print("Response content structure:", response.candidates[0].content.parts[0].text)
-
+    # 使用指定的 prompt 和內容來生成回應
+    response = model.generate_content(f"{prompt}: {content}")
+    
     # 提取生成的文本
     generated_text = response.candidates[0].content.parts[0].text
-    
-    return generated_text
+    formatted_text = re.sub(r'(\*\*[^\*]+\*\*)', r'\n\1', generated_text)
+    return formatted_text
+
 
 # 加入prompt優化分析的結果，probably things like: 根據回覆者的情緒統整出市場上使用者對該項產品歸納的優劣點
 # 加入分析回覆的內容
